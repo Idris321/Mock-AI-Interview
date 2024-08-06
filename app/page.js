48b5cd5821cca56a2e977af290ab06b131793d5c
Mujahid Shaikh <idrisisk12@gmail.com>
2024-08-06 20:09:09 +0530

    31

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div><h2>hi</h2>
+   <Button>hello</Button>
+   </div>
+  );
+}
